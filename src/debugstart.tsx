d062ccc1356db7e9132e4b46f50f06cf18ebc9da
Ilan Volow <ilanvolow@gmail.com>
2019-10-02 17:43:48 -0400

    Stopping point

--- src/debugstart.tsx
+++ src/debugstart.tsx
@@ -2,20 +2,11 @@ import { App, Route, Response, Param, JSON } from './utah';
 import HelloWorldHandler from './handlers/helloworld';
 
 <App>
-    <Route path='/translation'>
-        <JSON>
-            {{
-                'bed' : 'cama',
-                'butterfly' : 'mariposa',
-                'cow' : 'vaca'
-            }}
-        </JSON>
-    </Route>
-    <Route path='/candies'>
-        <JSON>
-           {
-               ['lollipops', 'jollyranchers']
-           }
-        </JSON>
-    </Route>
-</App>
+  <Route path='/hello' method='get' handler={ HelloWorldHandler }>
+     <Param name='person' datatype='string' paramtype='query'>
+          The name of the person
+     </Param>
+    <Response code='400' name='bunnySuccessReponse'/>
+    <Response code='200' name='successfulThreadResponse'/>
+  </Route>
+</App>;
