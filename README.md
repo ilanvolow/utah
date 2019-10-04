c4d7abbfe7c5964c4cbe9620b4975f09529633f8
Ilan Volow <ilanvolow@gmail.com>
2019-10-04 18:48:42 -0400

    Got tests working again

--- README.md
+++ README.md
@@ -27,15 +27,15 @@ Install the VSCode extension to get command autocompletion.
 Utah is designed to come back a number of shortcomings I've found with existing systems:
 
 
-1. It's really to much of a pain in the butt to set up Express for a 5 minute project.
-2. It's *really* a pain in the butt to set up Express to use Typescript.
-3. It's really a pain in the butt set up debugging for Typescript applications in Express that can be easily done in VSCode
-4. It's a pain in the butt to set up unit testing for Typescript for Express.
-5. Almost nobody sets up a json schema/Swagger/whatever document on their new project they've just started up on.
+1. It's really a pain in the butt to set up Express for a 5 minute project.
+2. It's really, really a pain in the butt to set up Express to use Typescript.
+3. It's really, really, realy a pain in the butt set up debugging for Typescript applications in Express that can be easily done in VSCode
+4. It's really, really, really, really a pain in the butt to set up unit testing for Typescript for Express.
+5. It's really, really, really, really, really a pain in the butt to get people on other teams to document their stuff in Swagger.
 
 Approach:
 
-1. Utah.ts is hyper-opinionated.
+1. Utah.ts is hyper-opinionated. You're weclome and our apologies, your pick.
 2. We require Visual Studio Code. Utah.ts is a system, not a framework.
 3. Utah demands you document your stuff up front.
 4. We use JSX. Stuff that never changes during execution shouldn't be in code, and JSX is enough not like code that it can meet this criteria for now.
--- src/__tests__/routing.test.tsx
+++ src/__tests__/routing.test.tsx
@@ -7,55 +7,26 @@ import { App, Route, Response, ResponseHandler } from '../utah'
 describe('Routing', () => {
     it('Can set up routes correctly', async () => {
 
-      class TestHandler extends ResponseHandler {
+      class TestHandler implements ResponseHandler {
         constructor() {
-          super();
+
         }
 
         public handle(request: express.Request, response: express.Response) {
-          expect(request.path).toEqual('/bunny');
-          expect(request.method).toEqual('GET');
-          expect(response).not.toBeUndefined;
-          response.status(this.responses['bunnySuccessResponse'].code).json({ title: 'Hello World'});
+          response.reply('bunnySuccessResponse', {'testOne': 'r'})
         }
       }
-
-      //let handler = new TestHandler();
 
       let appObject =
       <App>
         <Route path='/bunny' method='get' handler={ TestHandler }>
-          <Response code='200' name='bunnySuccessReponse'/>
+          <Response code='200' name='bunnySuccessResponse'/>
           <Response code='200' name='successfulThreadResponse'/>
         </Route>
       </App>;
 
       const response = await request(appObject.app).get('/bunny');
       expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({'testOne': 'r'});
     });
-
-    // it('Should throw an exception if path not set', () => {
-    //     expect(() => {
-    //       <App>
-    //         <Route method='get' handler={{}}/>
-    //       </App>
-    //     }).toThrow();
-    // });
-
-    // it('Should throw an exception if method not set', () => {
-    //   expect(() => {
-    //     <App>
-    //        <Route path='/bunny' handler={ TestHandler }/>
-    //     </App>
-    //   }).toThrow();
-    // });
-
-    // it('Should throw an exception if hander not set', () => {
-    //   expect(() => {
-    //     <App>
-    //        <Route path='/bunny' method='get'/>
-    //     </App>
-    //   })
-    //   .toThrow();
-    // });
   });
