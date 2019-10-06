94acae26d865d3b9738bf069c130ffb231d4b066
Ilan Volow <ilanvolow@gmail.com>
2019-10-05 21:33:47 -0400

    Adding/fixing more routing unit tests

--- src/__tests__/routing.test.tsx
+++ src/__tests__/routing.test.tsx
@@ -3,30 +3,42 @@ import express from 'express';
 
 import { App, Route, Response, ResponseHandler } from '../utah'
 
-
 describe('Routing', () => {
-    it('Can set up routes correctly', async () => {
 
-      class TestHandler implements ResponseHandler {
-        constructor() {
+    describe('setting up routes', () => {
+        class TestHandler implements ResponseHandler {
+          constructor() {
 
+          }
+          public handle(request: express.Request, response: express.Response) {
+            if (request.query.testSuccess) {
+              response.reply('successfulResponse', {'testOne': 'r'})
+            } else if (request.query.testFailure) {
+              response.reply('unsucessfulResponse', undefined);
+            }
+          }
         }
 
-        public handle(request: express.Request, response: express.Response) {
-          response.reply('bunnySuccessResponse', {'testOne': 'r'})
-        }
-      }
+        const appObject: App =
+        <App>
+          <Route path='/bunny' method='get' handler={ TestHandler }>
+            <Response code='200' name='successfulResponse'/>
+            <Response code='500' name='unsucessfulResponse'/>
+          </Route>
+        </App>;
 
-      let appObject =
-      <App>
-        <Route path='/bunny' method='get' handler={ TestHandler }>
-          <Response code='200' name='bunnySuccessResponse'/>
-          <Response code='200' name='successfulThreadResponse'/>
-        </Route>
-      </App>;
+      it('Can set up routes correctly', async () => {
+        const response = await request(appObject.app).get('/bunny?testSuccess=1');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({'testOne': 'r'});
+        appObject.server.close();
+      });
 
-      const response = await request(appObject.app).get('/bunny');
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toEqual({'testOne': 'r'});
+     it('Can set up routes correctly', async () => {
+        const response = await request(appObject.app).get('/bunny?testFailure=1');
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toEqual({});
+        appObject.server.close();
+      });
     });
   });
