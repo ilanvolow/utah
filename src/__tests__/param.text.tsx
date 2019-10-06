893297e717b31d142fcf73c12c46952f221a9105
Ilan Volow <ilanvolow@gmail.com>
2019-10-06 02:08:36 -0400

    Added Param unit tests

--- src/__tests__/param.text.tsx
+++ src/__tests__/param.text.tsx
@@ -1,21 +1,75 @@
 import request from 'supertest';
 import express from 'express';
 import { App, Route, Response, ResponseHandler, Param } from '../utah'
+import app from 'express/lib/application';
 
 describe('Query parameters', () => {
 
+  describe('construction', () => {
     class TestHandler implements ResponseHandler {
-        constructor() {
+      constructor() {
 
-        }
+      }
 
-        public handle(request: express.Request, response: express.Response) {
-          if (request.query.bunny_name === 'foofoo') {
-            response.reply('successfulResponse', {'testOne': 'r'})
-          } else {
-            response.reply('unsucessfulResponse', undefined);
-          }
+      public handle(request: express.Request, response: express.Response) {
+        response.reply('unsucessfulResponse', undefined);
+      }
+    }
+
+    it('should not allow a parameter to be created without a name', () => {
+      expect(() => {
+      const appObject: App =
+      <App>
+        <Route path='/bunny' method='get' handler={ TestHandler }>
+          <Param datatype='string' paramtype='query'/>
+          <Response code='200' name='successfulResponse'/>
+          <Response code='500' name='unsucessfulResponse'/>
+        </Route>
+      </App>;
+      }).toThrow();
+    });
+
+    it('should not allow a parameter to be created without a data type', () => {
+      expect(() => {
+      const app =
+      <App>
+        <Route path='/bunny' method='get' handler={ TestHandler }>
+          <Param name='bunny_name' paramtype='query'/>
+          <Response code='200' name='successfulResponse'/>
+          <Response code='500' name='unsucessfulResponse'/>
+        </Route>
+      </App>;
+      }).toThrow();
+    });
+
+    it('should not allow a parameter to be created without a param type', () => {
+      expect(() => {
+      const app =
+      <App>
+        <Route path='/bunny' method='get' handler={ TestHandler }>
+          <Param name='bunny_name' datatype='string'/>
+          <Response code='200' name='successfulResponse'/>
+          <Response code='500' name='unsucessfulResponse'/>
+        </Route>
+      </App>;
+      }).toThrow();
+    });
+  });
+  
+  describe('handling routes', () => {
+
+    class TestHandler implements ResponseHandler {
+      constructor() {
+
+      }
+
+      public handle(request: express.Request, response: express.Response) {
+        if (request.query.bunny_name === 'foofoo') {
+          response.reply('successfulResponse', {'testOne': 'r'})
+        } else {
+          response.reply('unsucessfulResponse', undefined);
         }
+      }
     }
 
     it('should successfully set up parameters and pass to the request', async () => {
@@ -33,7 +87,6 @@ describe('Query parameters', () => {
         expect(response.body).toEqual({'testOne': 'r'});
         appObject.server.close();
     });
-
 
     it('should handle optional parameters correctly', async () => {
       class OptionalParamHandler implements ResponseHandler {
@@ -59,30 +112,31 @@ describe('Query parameters', () => {
       expect(response.statusCode).toBe(200);
       expect(response.body).toEqual({'testOne': 'r'});
       appObject.server.close();
-  });
+    });
 
     it('should return a HTTP 400 error if an unexpected query parameter is sent', async () => {
-      class UndeclaredParamRejectionHandler implements ResponseHandler {
-        constructor() {
+        class UndeclaredParamRejectionHandler implements ResponseHandler {
+          constructor() {
 
+          }
+
+          public handle(request: express.Request, response: express.Response) {
+            response.reply('successfulResponse', {'testOne': 'r'})
+          }
         }
 
-        public handle(request: express.Request, response: express.Response) {
-          response.reply('successfulResponse', {'testOne': 'r'})
-        }
-      }
+        const appObject: App =
+        <App>
+          <Route path='/bunny' method='get' handler={ UndeclaredParamRejectionHandler }>
+            <Param name='bunny_name' datatype='string' paramtype='query'/> 
+            <Response code='200' name='successfulResponse'/>
+            <Response code='500' name='unsucessfulResponse'/>
+          </Route>
+        </App>;
 
-      const appObject: App =
-      <App>
-        <Route path='/bunny' method='get' handler={ UndeclaredParamRejectionHandler }>
-          <Param name='bunny_name' datatype='string' paramtype='query'/> 
-          <Response code='200' name='successfulResponse'/>
-          <Response code='500' name='unsucessfulResponse'/>
-        </Route>
-      </App>;
-
-      const response = await request(appObject.app).get('/bunny?myrandomvar=whatever');
-      expect(response.statusCode).toBe(400);
-      appObject.server.close();
+        const response = await request(appObject.app).get('/bunny?myrandomvar=whatever');
+        expect(response.statusCode).toBe(400);
+        appObject.server.close();
+      });
   });
 });
